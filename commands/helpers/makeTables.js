61c79969a1240d5940c51944ea4567cd5bdf9045
unknown <codymridener@gmail.com>
2019-10-05 03:48:31 -0400

    Gitignore test

--- commands/helpers/makeTables.js
+++ commands/helpers/makeTables.js
@@ -8,15 +8,20 @@ module.exports.run = async (db) => {
   db.query(`CREATE TABLE users(messageID varchar(80),userID varchar(80),name varchar(80),date varchar(80),channelID varchar(80), channel varchar(80),content text, embeds int, embedProviders text)`, (err,res) =>{
     if(err){
       console.log('users table already exists');
-      console.log(err);
+
     }
   });
   // Creates an embeds table that will store embed information for message embeds
-  // as well as any images, and image urls. 
+  // as well as any images, and image urls.
   db.query(`CREATE TABLE embeds(userID varchar(80), height int, width int, content text, date varchar(80),numEmbeds int, urls text[], paths text[] )`, (err,res) => {
     if(err){
       console.log('embeds table already exists');
-      console.log(err)
+
     }
   });
+  db.query(`create table commandTimer(userID varchar(80), command varchar(80), date varchar(80))`, (err,res) => {
+    if(err){
+      console.log('commandTimer already exists');
+    }
+  })
 }
